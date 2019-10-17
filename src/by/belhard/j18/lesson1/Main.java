a0de583cd3d5c393ff076129781deb90088e85eb
artdDaneliani <danelian_a@mail.ru>
2019-10-17 15:48:36 +0300

    task1

--- src/by/belhard/j18/lesson1/Main.java
+++ src/by/belhard/j18/lesson1/Main.java
@@ -1,5 +1,11 @@
 package by.belhard.j18.lesson1;
 
-public static void main(String[]args){
+public class Main {
+    public static void main(String[] args) {
 
-        }
+        int g = 50;
+        int v = 230;
+        System.out.println( g + v );
+
+    }
+}
