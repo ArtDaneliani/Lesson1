a53f42b125d63281c26985b656c02728d1f8990f
artificalBrain <danelian_a@mail.ru>
2019-10-17 19:36:21 +0300

    hwk

--- src/by/belhard/j18/lesson2/task4/task4.java
+++ src/by/belhard/j18/lesson2/task4/task4.java
@@ -2,9 +2,7 @@ package by.belhard.j18.lesson2.task4;
 
 public class task4 {
     public static void main(String[] args) {
-
         int =20;
-
         if (a < 20) {
             System.out.println(a);
         } else if (a >= 20 && a < 60)
